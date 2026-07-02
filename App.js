7d6adc0174bbf0ffc1fc979d0ef0ef9f994e610f
Jan Cihlář <cihlarhonza@seznam.cz>
2026-07-02 20:29:14 +0200

    Main file

---
+++ App.js
@@ -0,0 +1,20 @@
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import CartProvider from './store/CartProvider';
+
+function App() {
+  return (
+    <CartProvider>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
+
+function jarda() {
+  return 50
+}
