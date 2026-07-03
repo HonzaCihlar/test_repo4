6b38181e7f5b2a91fe33191aab393486d4a67c79
Jan Cihlář <cihlarhonza@seznam.cz>
2026-07-03 10:06:31 +0200

    Final version

--- App.js
+++ App.js
@@ -16,42 +16,10 @@ function App() {
 export default App;
 
 function jarda() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-  return 1
-<<<<<<< HEAD
-}
-
-function jarda2() {
-  return 2
-=======
-=======
-  return 2
->>>>>>> 8da9693 (Vetev 2 file)
->>>>>>> 86bc3d3 (Vetev 2 file)
-=======
-  return 2
->>>>>>> 2582bfc (Resolve conflict in App.js)
-=======
-  return 2
-=======
   return 1
 }
 
+
 function jarda2() {
-<<<<<<< HEAD
   return 1
->>>>>>> f83b8e7 (Added a new function)
-<<<<<<< HEAD
->>>>>>> 5703b3d (Added a new function)
-=======
-=======
-  return 2
->>>>>>> e860c5b (second function value change)
->>>>>>> 65564c0 (second function value change)
-=======
-  return 2
->>>>>>> origin/master
 }
